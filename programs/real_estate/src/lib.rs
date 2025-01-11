f3f0290edae9f7e1379fb7dfea49302b623dab7f
ayoubbijarche <ayoubbijarche6@gmail.com>
2025-01-11 21:29:38 +0100

    sum changes

--- programs/real_estate/src/lib.rs
+++ programs/real_estate/src/lib.rs
@@ -1,24 +1,34 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::Transfer;
+use anchor_spl::token::{Token};
 
 declare_id!("GCPRCAMZWnTCtawVi6wMuXNy9Rdja6dvoastFFPkrBd3");
 
 #[program]
 pub mod real_estate {
 
+
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
     
-    pub fn create_property(ctx : Context<InitializeProperty> , property_data : PropertyData) -> Result<()> {
+    pub fn create_property(ctx : Context<InitializeProperty> , property_data : PropertyData , nft_mint : Pubkey) -> Result<()> {
         let property = &mut ctx.accounts.property;
-        let owner = property.owner;
         property.property = property_data;
+        property.owner = property.owner.key();
+        property.nft_mint = nft_mint;
         Ok(())
     }
 
-    pub fn buy_property(ctx : Context<InitializeProperty>) -> Result<()> {
-        
+    pub fn buy_property(ctx : Context<BuyProperty> , price : u64) -> Result<()> {
+        let property = &mut ctx.accounts.property;
+        let transfer_instruction = Transfer{
+            from : ctx.accounts.seller,
+            to : ctx.accounts.buyer,
+            authority : ctx.accounts.buyer
+        };
+        Ok(())
     }
 }
 
@@ -39,6 +49,24 @@ pub struct InitializeProperty<'info>{
     pub system_program : Program<'info , System>
 }
 
+#[derive(Accounts)]
+pub struct BuyProperty<'info> {
+    #[account(mut)]
+    pub property: Account<'info, Property>,
+    #[account(mut)]
+    pub buyer: Signer<'info>,
+    #[account(mut)]
+    pub seller: SystemAccount<'info>,
+    /// CHECK: Safe because we only use it for CPI
+    pub system_program: Program<'info, System>,
+    /// CHECK: Safe because we only use it for CPI
+    pub token_program: Program<'info, Token>,
+    #[account(mut, constraint = seller_token_account.owner == seller.key())]
+    pub seller_token_account: Account<'info, anchor_spl::token::TokenAccount>,
+    #[account(mut)]
+    pub buyer_token_account: Account<'info, anchor_spl::token::TokenAccount>,
+}
+
 
 #[account]
 pub struct PropertyData{
@@ -55,8 +83,14 @@ pub struct PropertyData{
     pub image_url : String
 }
 
+
 #[account]
 pub struct Property{
     pub owner : Pubkey,
+    pub nft_mint : Pubkey,
     pub property : PropertyData
 }
+
+
+
+
